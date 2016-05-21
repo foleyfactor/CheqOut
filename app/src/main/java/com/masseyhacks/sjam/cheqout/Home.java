b369b14a4992c24d0bfc26b869434f172726930a
Alex Foley <foleyfactor98@gmail.com>
2016-05-21 15:55:07 -0400

    Firebase can post; working on accessing.

--- app/src/main/java/com/masseyhacks/sjam/cheqout/Home.java
+++ app/src/main/java/com/masseyhacks/sjam/cheqout/Home.java
@@ -1,22 +1,42 @@
 package com.masseyhacks.sjam.cheqout;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.SQLOutput;
 
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseDatabase f = FirebaseDatabase.getInstance();
+        DatabaseReference ref = f.getReference();
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Context c = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                String val = dataSnapshot.getValue(String.class);
+                Toast t = Toast.makeText(c, val, duration);
+                t.show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         setContentView(R.layout.activity_home);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("test");
-
-        myRef.setValue("lol pls");
     }
 }
