b05100d33c1dd8fe8e1bd0afa97d6bd9882d4512
Alex Foley <foleyfactor98@gmail.com>
2016-05-21 15:11:03 -0400

    Added Firebase dependencies.

--- app/src/main/java/com/masseyhacks/sjam/cheqout/Home.java
+++ app/src/main/java/com/masseyhacks/sjam/cheqout/Home.java
@@ -3,11 +3,20 @@ package com.masseyhacks.sjam.cheqout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("test");
+
+        myRef.setValue("lol pls");
     }
 }
