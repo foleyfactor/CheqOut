88364ffaf0fb8b5416b2d47e1050c67687142bc2
Alex Foley <foleyfactor98@gmail.com>
2016-05-21 16:12:20 -0400

    Added google services

--- app/src/main/java/com/masseyhacks/sjam/cheqout/Home.java
+++ app/src/main/java/com/masseyhacks/sjam/cheqout/Home.java
@@ -9,25 +9,27 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceIdService;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Home extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FirebaseDatabase f = FirebaseDatabase.getInstance();
-        DatabaseReference ref = f.getReference();
+    private FirebaseDatabase firebase;
+    private DatabaseReference ref;
 
-        ref.addValueEventListener(new ValueEventListener() {
+    public ArrayList getDataFromFirebase(String s) {
+        ref.child("Stores").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList values = (ArrayList) dataSnapshot.getValue();
                 Context c = getApplicationContext();
-                int duration = Toast.LENGTH_SHORT;
-                String val = dataSnapshot.getValue(String.class);
-                Toast t = Toast.makeText(c, val, duration);
+                int d = Toast.LENGTH_SHORT;
+                Toast t = Toast.makeText(c, (String) values.get(0), d);
                 t.show();
             }
 
@@ -36,6 +38,31 @@ public class Home extends AppCompatActivity {
 
             }
         });
+    }
+
+    public ArrayList getDataFromFirebase(String s1, String s2) {
+        ref.child(s1).child(s2).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList values = (ArrayList) dataSnapshot.getValue();
+                Context c = getApplicationContext();
+                int d = Toast.LENGTH_SHORT;
+                Toast t = Toast.makeText(c, (String) values.get(0), d);
+                t.show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firebase = FirebaseDatabase.getInstance();
+        ref = firebase.getReference();
 
         setContentView(R.layout.activity_home);
     }
