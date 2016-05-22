f7523eb6ef11f332512d2fa4ce1b32bc9312af99
Ethan Guo <ethguo@gmail.com>
2016-05-22 11:02:27 -0400

    I can't what I changed but apparently I changed a bunch of stuff

---
+++ app/src/main/java/com/masseyhacks/sjam/cheqout/PaymentActivity.java
@@ -0,0 +1,128 @@
+package com.masseyhacks.sjam.cheqout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.BooleanResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wallet.Cart;
+import com.google.android.gms.wallet.FullWalletRequest;
+import com.google.android.gms.wallet.LineItem;
+import com.google.android.gms.wallet.MaskedWallet;
+import com.google.android.gms.wallet.MaskedWalletRequest;
+import com.google.android.gms.wallet.PaymentMethodTokenizationParameters;
+import com.google.android.gms.wallet.PaymentMethodTokenizationType;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+import com.google.android.gms.wallet.fragment.SupportWalletFragment;
+import com.google.android.gms.wallet.fragment.WalletFragmentInitParams;
+
+public class PaymentActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    public static String TAG = "StripePayment";
+
+    private GoogleApiClient googleApiClient;
+
+    // You will need to use your live API key even while testing
+    public static final String PUBLISHABLE_KEY = "pk_live_XXX";
+
+    // Unique identifiers for asynchronous requests:
+    private static final int LOAD_MASKED_WALLET_REQUEST_CODE = 1000;
+    private static final int LOAD_FULL_WALLET_REQUEST_CODE = 1001;
+
+    private SupportWalletFragment walletFragment;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Wallet.Payments.isReadyToPay(googleApiClient).setResultCallback(
+                new ResultCallback<BooleanResult>() {
+                    @Override
+                    public void onResult(@NonNull BooleanResult booleanResult) {
+                        if (booleanResult.getStatus().isSuccess()) {
+                            if (booleanResult.getValue()) {
+                                showAndroidPay();
+                            } else {
+                                // Hide Android Pay buttons, show a message that Android Pay
+                                // cannot be used yet, and display a traditional checkout button
+                            }
+                        } else {
+                            // Error making isReadyToPay call
+                            Log.e(TAG, "isReadyToPay:" + booleanResult.getStatus());
+                        }
+                    }
+                });
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Wallet.API, new Wallet.WalletOptions.Builder()
+                        .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+                        .setTheme(WalletConstants.THEME_LIGHT)
+                        .build())
+                .build();
+    }
+
+    public void showAndroidPay() {
+        setContentView(R.layout.activity_payment);
+
+        walletFragment =
+                (SupportWalletFragment) getSupportFragmentManager().findFragmentById(R.id.wallet_fragment);
+
+        MaskedWalletRequest maskedWalletRequest = MaskedWalletRequest.newBuilder()
+
+                // Request credit card tokenization with Stripe by specifying tokenization parameters:
+                .setPaymentMethodTokenizationParameters(PaymentMethodTokenizationParameters.newBuilder()
+                        .setPaymentMethodTokenizationType(PaymentMethodTokenizationType.PAYMENT_GATEWAY)
+                        .addParameter("gateway", "stripe")
+                        .addParameter("stripe:publishableKey", PUBLISHABLE_KEY)
+                        .addParameter("stripe:version", com.stripe.Stripe.VERSION)
+                        .build())
+
+                // You want the shipping address:
+                .setShippingAddressRequired(true)
+
+                // Price set as a decimal:
+                .setEstimatedTotalPrice("20.00")
+                .setCurrencyCode("USD")
+                .build();
+
+        // Set the parameters:
+        WalletFragmentInitParams initParams = WalletFragmentInitParams.newBuilder()
+                .setMaskedWalletRequest(maskedWalletRequest)
+                .setMaskedWalletRequestCode(LOAD_MASKED_WALLET_REQUEST_CODE)
+                .build();
+
+        // Initialize the fragment:
+        walletFragment.initialize(initParams);
+    }
+
+    public void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    public void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO: Android Pay
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {}
+
+    @Override
+    public void onConnected(Bundle bundle) {}
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+}
